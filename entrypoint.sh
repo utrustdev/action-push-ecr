383fbedef9abaeeb0cf15363798145662474ad94
Miguel Palhas <mpalhas@gmail.com>
2019-12-04 18:20:17 +0000

    Fixes git tag usage

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 repo=$1
 tag=$2
 dockerfile=$3
+parsed_tag=$(echo $tag | sed 's|.*/||')
 account_url=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
@@ -10,6 +11,6 @@ eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
 if aws ecr describe-images --repository-name $repo --image-ids imageTag=$tag --region $AWS_REGION > /dev/null 2>&1; then
   echo "The image $tag is already in the registry"
 else
-  docker build -f $dockerfile -t "$account_url/$repo:$tag" .
-  docker push "$account_url/$repo:$tag"
+  docker build -f $dockerfile -t "$account_url/$repo:$parsed_tag" .
+  docker push "$account_url/$repo:$parsed_tag"
 fi
