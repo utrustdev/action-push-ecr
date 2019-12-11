e0eefc77e3e66fdb9f27e54d29ba757365cbb357
Miguel Palhas <mpalhas@gmail.com>
2019-12-11 11:13:04 +0000

    Adds support for extra-build-args

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 repo=$1
 tag=$2
 dockerfile=$3
+extra_build_args="$4"
 parsed_tag=$(echo $tag | sed 's|.*/||')
 account_url=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
@@ -11,6 +12,6 @@ eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
 if aws ecr describe-images --repository-name $repo --image-ids imageTag=$tag --region $AWS_REGION > /dev/null 2>&1; then
   echo "The image $tag is already in the registry"
 else
-  docker build -f $dockerfile -t "$account_url/$repo:$parsed_tag" .
+  docker build -f $dockerfile $extra_build_args -t "$account_url/$repo:$parsed_tag" .
   docker push "$account_url/$repo:$parsed_tag"
 fi
