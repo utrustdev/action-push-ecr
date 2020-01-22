b83d9d5fc3c866c83dda485e645e9ee2bb2173b6
Miguel Palhas <mpalhas@gmail.com>
2020-01-22 18:09:53 +0000

    Fixing check for image already built

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ account_url=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
 
-if aws ecr describe-images --repository-name $repo --image-ids imageTag=$tag --region $AWS_REGION > /dev/null 2>&1; then
+if aws ecr describe-images --repository-name $repo --image-ids imageTag=$parsed_tag --region $AWS_REGION > /dev/null 2>&1; then
   echo "The image $tag is already in the registry"
 else
   docker build -f $dockerfile $extra_build_args -t "$account_url/$repo:$parsed_tag" .
